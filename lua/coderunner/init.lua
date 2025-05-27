2093a90bf0e4b69f8575d389debd76a05bae1fda
erikbloodshed <erik.bloodshed@hotmail.com>
2025-05-28 00:12:40 +0800

    Refactor coderunner, remove unnecessary messages.

--- lua/coderunner/init.lua
+++ lua/coderunner/init.lua
@@ -1,16 +1,15 @@
 -- lua/runner.lua (Main plugin file)
-local config_module = require('coderunner.config')
-local executor = require('coderunner.executor')
-local log = vim.notify
-
 local M = {}
 
 -- Default group for autocommands to ensure they can be cleared.
-local AUGROUP_NAME = "RunnerFileTypeSetup"
 
 --- Sets up the runner plugin with user-provided configuration.
 -- @param user_config table|nil User configuration to override defaults.
 function M.setup(user_config)
+    local config_module = require('coderunner.config')
+    local executor = require('coderunner.executor')
+    local log = vim.notify
+
     config_module.setup(user_config)
     local current_config = config_module.get()
 
@@ -33,7 +32,7 @@ function M.setup(user_config)
     end
 
     if #supported_filetypes > 0 then
-        local group = vim.api.nvim_create_augroup(AUGROUP_NAME, { clear = true })
+        local group = vim.api.nvim_create_augroup("RunnerFileTypeSetup", { clear = true })
         vim.api.nvim_create_autocmd("FileType", {
             pattern = supported_filetypes,
             group = group,
@@ -54,33 +53,6 @@ function M.setup(user_config)
     else
         log("No runners configured, skipping autocommand setup.", vim.log.levels.INFO)
     end
-end
-
---- Adds or updates a custom runner configuration for a given filetype.
--- @param filetype string The filetype (e.g., "go", "rust").
--- @param runner_cfg table The configuration for this runner.
-function M.add_runner(filetype, runner_cfg)
-    config_module.add_runner(filetype, runner_cfg)
-    log(
-    "Added/Updated runner for " ..
-    filetype ..
-    ". You might need to re-run M.setup() or restart Neovim for keymap changes to apply to new filetypes if the FileType event was already triggered.",
-        vim.log.levels.INFO)
-    -- To make new filetypes immediately active for autocommands, setup would need to be recalled
-    -- or a more dynamic autocommand registration would be needed.
-    -- For simplicity, if a new filetype is added, user might need to trigger FileType event again or call setup.
-end
-
---- Runs the file in the current buffer based on its filetype.
--- Can be called manually.
-function M.run()
-    executor.run_current_file(vim.api.nvim_get_current_buf())
-end
-
---- Retrieves the current merged configuration of the plugin.
--- @return table The current configuration.
-function M.get_config()
-    return config_module.get()
 end
 
 return M
