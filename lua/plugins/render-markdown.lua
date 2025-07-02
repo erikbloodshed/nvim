2a7a877df22f4c2c1f4c490fd844187a777ba013
erikbloodshed <erik.bloodshed@hotmail.com>
2025-07-03 07:35:10 +0800

    modified:   lua/plugins/render-markdown.lua

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,4 +1,9 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = "markdown",
+    config = function()
+        require('render-markdown').setup({
+            anti_conceal = { enabled = false },
+        })
+    end
 }
