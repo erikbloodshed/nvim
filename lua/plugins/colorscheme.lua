59004a2a948f6b664345da2e0badc657a104ac80
erikbloodshed <erik.bloodshed@hotmail.com>
2025-08-02 11:49:24 +0800

    modified:   lua/plugins/colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -54,11 +54,15 @@ return {
         config = function()
             require("catppuccin").setup({
                 flavour = "mocha", -- latte, frappe, macchiato, mocha
-                background = {    -- :h background
+                background = {     -- :h background
                     light = "latte",
                     dark = "mocha",
                 },
                 transparent_background = true, -- disables setting the background color.
+                float = {
+                    transparent = true,       -- enable transparent floating windows
+                    solid = true,             -- use solid styling for floating windows, see |winborder|
+                },
                 show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
                 term_colors = true,            -- sets terminal colors (e.g. `g:terminal_color_0`)
                 dim_inactive = {
