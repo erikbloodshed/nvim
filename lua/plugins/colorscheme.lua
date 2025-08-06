be19ba9a57959534e5444139497f0e08c490e394
erikbloodshed <erik.bloodshed@hotmail.com>
2025-08-06 12:00:36 +0800

    Improvements

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,28 +4,28 @@ return {
     priority = 1000,
     config = function()
         require("catppuccin").setup({
-            flavour = "mocha",     -- latte, frappe, macchiato, mocha
-            background = {         -- :h background
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            background = {     -- :h background
                 light = "latte",
                 dark = "mocha",
             },
-            transparent_background = true,     -- disables setting the background color.
+            transparent_background = true, -- disables setting the background color.
             float = {
-                transparent = true,            -- enable transparent floating windows
-                solid = true,                  -- use solid styling for floating windows, see |winborder|
+                transparent = true,        -- enable transparent floating windows
+                solid = true,              -- use solid styling for floating windows, see |winborder|
             },
-            show_end_of_buffer = false,        -- shows the '~' characters after the end of buffers
-            term_colors = true,                -- sets terminal colors (e.g. `g:terminal_color_0`)
+            show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+            term_colors = true,            -- sets terminal colors (e.g. `g:terminal_color_0`)
             dim_inactive = {
-                enabled = false,               -- dims the background color of inactive window
+                enabled = false,           -- dims the background color of inactive window
                 shade = "dark",
-                percentage = 0.15,             -- percentage of the shade to apply to the inactive window
+                percentage = 0.15,         -- percentage of the shade to apply to the inactive window
             },
-            no_italic = false,                 -- Force no italic
-            no_bold = false,                   -- Force no bold
-            no_underline = false,              -- Force no underline
-            styles = {                         -- Handles the styles of general hi groups (see `:h highlight-args`):
-                comments = { "italic" },       -- Change the style of comments
+            no_italic = false,             -- Force no italic
+            no_bold = false,               -- Force no bold
+            no_underline = false,          -- Force no underline
+            styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" },   -- Change the style of comments
                 conditionals = {},
                 loops = {},
                 functions = { "bold" },
@@ -37,7 +37,6 @@ return {
                 properties = {},
                 types = {},
                 operators = {},
-                -- miscs = {}, -- Uncomment to turn off hard-coded styles
             },
             color_overrides = {},
             custom_highlights = {},
@@ -54,7 +53,6 @@ return {
             }
         })
 
-        -- setup must be called before loading
         vim.cmd.colorscheme "catppuccin"
     end
 }
