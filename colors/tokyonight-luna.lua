5659e603233e4200e5feb42a81c24c0f369e60d4
erikbloodshed <erik.bloodshed@hotmail.com>
2025-08-19 00:55:27 +0800

    Refactoring tokyonight-luna is done, almost.

--- colors/tokyonight-luna.lua
+++ colors/tokyonight-luna.lua
@@ -1,18 +1,3 @@
---- DISCLAIMER
---- This colorscheme is a derivative work based on tokyonight-moon, a theme
---- created by Folke Lemaitre as part of the tokyonight.nvim suite. While some
---- colors have been modified, the main goal of this refactor is to simplify the
---- colorscheme for personal use while retaining the overall aesthetic and design
---- principles of the original. The core palette and design philosophy originate
---- from Lemaitre’s work.
---- The original tokyonight.nvim project is licensed under the Apache 2.0 License.
---- In accordance with its terms, this derivative work gives full credit to the
---- original author. Please find the original source and its license here:
---- @link: https://github.com/folke/tokyonight.nvim
---- @author: Folke Lemaitre
---- All credit for the foundational design and color theory belongs to him.
-local bg_clear = true
-
 vim.cmd.highlight("clear")
 vim.cmd.syntax("reset")
 
@@ -21,4 +6,4 @@ vim.o.termguicolors = true
 vim.g.colors_name = "tokyonight-luna"
 vim.g.matchparen_disable_cursor_hl = 1
 
-require("themes.tokyonight-luna").init(bg_clear)
+require("themes.tokyonight-luna").init(true)
--- lua/themes/tokyonight-luna/init.lua
+++ lua/themes/tokyonight-luna/init.lua
@@ -1,32 +1,32 @@
-local colors = require("themes.tokyonight-luna.colors")
-local term_colors = colors.terminal
+local c = require("themes.tokyonight-luna.colors")
+local tc = c.terminal
 local g = vim.g
 local M = {}
 
 M.init = function(bg_clear)
-  require("themes.tokyonight-luna.schema.base").get(colors, bg_clear)
-  require("themes.tokyonight-luna.schema.treesitter").get(colors)
-  require("themes.tokyonight-luna.schema.semantic-token").get(colors)
-  require("themes.tokyonight-luna.schema.blink").get(colors)
-  require("themes.tokyonight-luna.schema.neotree").get(colors)
-  require("themes.tokyonight-luna.schema.render-markdown").get(colors)
+  require("themes.tokyonight-luna.schema.base").get(c, bg_clear)
+  require("themes.tokyonight-luna.schema.treesitter").get(c)
+  require("themes.tokyonight-luna.schema.semantic-token").get(c)
+  require("themes.tokyonight-luna.schema.blink").get(c)
+  require("themes.tokyonight-luna.schema.neotree").get(c)
+  require("themes.tokyonight-luna.schema.render-markdown").get(c)
 
-  g.terminal_color_0  = term_colors.black
-  g.terminal_color_1  = term_colors.black_bright
-  g.terminal_color_2  = term_colors.red
-  g.terminal_color_3  = term_colors.red_bright
-  g.terminal_color_4  = term_colors.green
-  g.terminal_color_5  = term_colors.green_bright
-  g.terminal_color_6  = term_colors.yellow
-  g.terminal_color_7  = term_colors.yellow_bright
-  g.terminal_color_8  = term_colors.blue
-  g.terminal_color_9  = term_colors.blue_bright
-  g.terminal_color_10 = term_colors.magenta
-  g.terminal_color_11 = term_colors.magenta_bright
-  g.terminal_color_12 = term_colors.cyan
-  g.terminal_color_13 = term_colors.cyan_bright
-  g.terminal_color_14 = term_colors.white
-  g.terminal_color_15 = term_colors.white_bright
+  g.terminal_color_0  = tc.black
+  g.terminal_color_1  = tc.red
+  g.terminal_color_2  = tc.green
+  g.terminal_color_3  = tc.yellow
+  g.terminal_color_4  = tc.blue
+  g.terminal_color_5  = tc.magenta
+  g.terminal_color_6  = tc.cyan
+  g.terminal_color_7  = tc.white
+  g.terminal_color_8  = tc.black_bright
+  g.terminal_color_9  = tc.red_bright
+  g.terminal_color_10 = tc.green_bright
+  g.terminal_color_11 = tc.yellow_bright
+  g.terminal_color_12 = tc.blue_bright
+  g.terminal_color_13 = tc.magenta_bright
+  g.terminal_color_14 = tc.cyan_bright
+  g.terminal_color_15 = tc.white_bright
 end
 
 return M
