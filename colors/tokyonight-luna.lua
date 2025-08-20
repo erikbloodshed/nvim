ad65e4c32ac27097fb1e8898b47bcdc59e9b41da
erikbloodshed <erik.bloodshed@hotmail.com>
2025-08-20 08:12:56 +0800

    set background

--- colors/tokyonight-luna.lua
+++ colors/tokyonight-luna.lua
@@ -3,6 +3,7 @@ vim.cmd.syntax("reset")
 
 ---@format disable-next
 vim.o.termguicolors = true
+vim.o.background = "dark"
 vim.g.colors_name = "tokyonight-luna"
 vim.g.matchparen_disable_cursor_hl = 1
 
