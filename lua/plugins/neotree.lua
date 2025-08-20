ca5e26e52c5e6f6142d985471a1c7bdec9770614
erikbloodshed <erik.bloodshed@hotmail.com>
2025-08-21 07:15:17 +0800

    Improve neotree cursor hiding logic

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,18 +1,3 @@
-local api = vim.api
-local opt = vim.opt
-
-local hl = api.nvim_get_hl(0, { name = "Cursor", link = false })
-
-local function hide_cursor()
-  api.nvim_set_hl(0, "Cursor", { blend = 100, fg = hl.fg, bg = hl.bg })
-  opt.guicursor:append("a:Cursor/lCursor")
-end
-
-local function show_cursor()
-  api.nvim_set_hl(0, "Cursor", { blend = 0, fg = hl.fg, bg = hl.bg })
-  opt.guicursor:remove("a:Cursor/lCursor")
-end
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
 
@@ -27,6 +12,18 @@ return {
         })
       end,
     },
+
+    {
+      "<C-,>",
+      function()
+        require("neo-tree.command").execute({
+          dir = "/home/xenyan/.config/nvim",
+          toggle = true,
+          reveal = true,
+          reveal_force_cwd = true,
+        })
+      end,
+    },
   },
 
   dependencies = {
@@ -35,44 +32,60 @@ return {
     "MunifTanjim/nui.nvim",
   },
 
-  opts = {
-    source_selector = {
-      winbar = false,
-      statusline = false,
-    },
-    close_if_last_window = true,
-    popup_border_style = "rounded",
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = { enabled = true },
-      filtered_items = {
-        hide_by_pattern = { "*.out" },
+  config = function()
+    local api = vim.api
+    local opt = vim.opt
+
+    local hl = api.nvim_get_hl(0, { name = "Cursor", link = false })
+    opt.guicursor:append("a:Cursor/lCursor")
+
+    local function hide_cursor()
+      api.nvim_set_hl(0, "Cursor", { blend = 100, fg = hl.fg, bg = hl.bg })
+    end
+
+    local function show_cursor()
+      api.nvim_set_hl(0, "Cursor", { blend = 0, fg = hl.fg, bg = hl.bg })
+    end
+
+    require("neo-tree").setup({
+      source_selector = {
+        winbar = false,
+        statusline = false,
       },
-      use_libuv_file_watcher = false,
-    },
-    event_handlers = {
-      {
-        event = "neo_tree_buffer_enter",
-        handler = hide_cursor
+      close_if_last_window = true,
+      popup_border_style = "rounded",
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        filtered_items = {
+          hide_by_pattern = { "*.out" },
+        },
+        use_libuv_file_watcher = false,
       },
-      {
-        event = "neo_tree_buffer_leave",
-        handler = function() show_cursor() end,
+      event_handlers = {
+        {
+          event = "neo_tree_buffer_enter",
+          handler = hide_cursor
+        },
+        {
+          event = "neo_tree_buffer_leave",
+          handler = show_cursor,
+        },
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+        {
+          event = "neo_tree_popup_input_ready",
+          handler = show_cursor
+        },
+        {
+          event = "neo_tree_popup_buffer_enter",
+          handler = show_cursor,
+        },
       },
-      {
-        event = "file_opened",
-        handler = function()
-          require("neo-tree.command").execute({ action = "close" })
-        end,
-      },
-      {
-        event = "neo_tree_popup_input_ready",
-        handler = show_cursor
-      },
-      {
-        event = "neo_tree_popup_buffer_enter",
-        handler = show_cursor,
-      },
-    },
-  },
+    })
+  end
 }
