bb5907987976bdc606727835ba9b6a61e0964f8f
xenyan <xenyan.matulac@gmail.com>
2025-08-27 13:49:13 +0800

    refactor: Modularize colorscheme

---
+++ lua/themes/decaf/colors.lua
@@ -0,0 +1,64 @@
+local M = {}
+
+local pat = "^#([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])$"
+local rgb = function(hex_str)
+  local red, green, blue = string.match(string.lower(hex_str), pat)
+  return { tonumber(red, 16), tonumber(green, 16), tonumber(blue, 16) }
+end
+
+local darken = function(f, a, b)
+  b, f = rgb(b), rgb(f)
+  local blendChannel = function(i)
+    local ret = (a * f[i] + ((1 - a) * b[i]))
+    return math.floor(math.min(math.max(0, ret), 255) + 0.5)
+  end
+
+  return string.format("#%02X%02X%02X", blendChannel(1), blendChannel(2), blendChannel(3))
+end
+
+
+M = {
+  rosewater = "#f5e0dc",
+  flamingo  = "#f2cdcd",
+  pink      = "#f5c2e7",
+  mauve     = "#cba6f7",
+  red       = "#f38ba8",
+  maroon    = "#eba0ac",
+  peach     = "#fab387",
+  yellow    = "#f9e2af",
+  green     = "#a6e3a1",
+  teal      = "#94e2d5",
+  sky       = "#89dceb",
+  sapphire  = "#74c7ec",
+  blue      = "#89b4fa",
+  lavender  = "#b4befe",
+  text      = "#cdd6f4",
+  subtext1  = "#bac2de",
+  subtext0  = "#a6adc8",
+  overlay2  = "#9399b2",
+  overlay1  = "#7f849c",
+  overlay0  = "#6c7086",
+  surface2  = "#585b70",
+  surface1  = "#45475a",
+  surface0  = "#313244",
+  base      = "#1e1e2e",
+  mantle    = "#181825",
+  crust     = "#11111b",
+  none      = "NONE"
+}
+
+M.dim = darken(M.base, 0.15, M.mantle)
+M.bg_dvt_error = darken(M.red, 0.095, M.base)
+M.bg_dvt_warn = darken(M.yellow, 0.095, M.base)
+M.bg_dvt_info = darken(M.sky, 0.095, M.base)
+M.bg_dvt_hint = darken(M.teal, 0.095, M.base)
+M.bg_dvt_ok = darken(M.green, 0.095, M.base)
+M.bg_line = darken(M.surface0, 0.64, M.base)
+M.bg_diff_add = darken(M.green, 0.18, M.base)
+M.bg_diff_change = darken(M.blue, 0.07, M.base)
+M.bg_diff_delete = darken(M.red, 0.18, M.base)
+M.bg_diff_text = darken(M.blue, 0.30, M.base)
+M.bg_search = darken(M.sky, 0.30, M.base)
+M.bg_incsearch = darken(M.sky, 0.90, M.base)
+
+return M
--- lua/themes/decaf/init.lua
+++ lua/themes/decaf/init.lua
@@ -1,13 +1,11 @@
 local g, o, api = vim.g, vim.o, vim.api
 
-o.termguicolors = true
-o.background = "dark"
-
 if g.colors_name then
   vim.cmd.highlight("clear")
 end
 
 g.colors_name = "decaf"
+o.termguicolors = true
 
 local cfg = {
   transparency = false,
@@ -17,91 +15,26 @@ local cfg = {
   },
 }
 
-local c = {
-  rosewater = "#f5e0dc",
-  flamingo  = "#f2cdcd",
-  pink      = "#f5c2e7",
-  mauve     = "#cba6f7",
-  red       = "#f38ba8",
-  maroon    = "#eba0ac",
-  peach     = "#fab387",
-  yellow    = "#f9e2af",
-  green     = "#a6e3a1",
-  teal      = "#94e2d5",
-  sky       = "#89dceb",
-  sapphire  = "#74c7ec",
-  blue      = "#89b4fa",
-  lavender  = "#b4befe",
-  text      = "#cdd6f4",
-  subtext1  = "#bac2de",
-  subtext0  = "#a6adc8",
-  overlay2  = "#9399b2",
-  overlay1  = "#7f849c",
-  overlay0  = "#6c7086",
-  surface2  = "#585b70",
-  surface1  = "#45475a",
-  surface0  = "#313244",
-  base      = "#1e1e2e",
-  mantle    = "#181825",
-  crust     = "#11111b",
-  none      = "NONE"
+local c = require("themes.decaf.colors")
+
+local mod = {
+  require("themes.decaf.schema.editor").get(c, cfg),
+  require("themes.decaf.schema.native_lsp").get(c, cfg),
+  require("themes.decaf.schema.neotree").get(c, cfg),
+  require("themes.decaf.schema.syntax").get(c, cfg),
+  require("themes.decaf.schema.treesitter").get(c),
+  require("themes.decaf.schema.semantic_tokens").get(c),
+  require("themes.decaf.schema.blink").get(c),
 }
 
-local function apply_highlights()
-  local pat = "^#([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])$"
-
-  local rgb = function(hex_str)
-    local red, green, blue = string.match(string.lower(hex_str), pat)
-    return { tonumber(red, 16), tonumber(green, 16), tonumber(blue, 16) }
-  end
-
-  local darken = function(f, a, b)
-    b, f = rgb(b), rgb(f)
-    local blendChannel = function(i)
-      local ret = (a * f[i] + ((1 - a) * b[i]))
-      return math.floor(math.min(math.max(0, ret), 255) + 0.5)
-    end
-
-    return string.format("#%02X%02X%02X", blendChannel(1), blendChannel(2), blendChannel(3))
-  end
-
-  c.dim = darken(c.base, 0.15, c.mantle)
-  c.bg_dvt_error = darken(c.red, 0.095, c.base)
-  c.bg_dvt_warn = darken(c.yellow, 0.095, c.base)
-  c.bg_dvt_info = darken(c.sky, 0.095, c.base)
-  c.bg_dvt_hint = darken(c.teal, 0.095, c.base)
-  c.bg_dvt_ok = darken(c.green, 0.095, c.base)
-  c.bg_line = darken(c.surface0, 0.64, c.base)
-  c.bg_diff_add = darken(c.green, 0.18, c.base)
-  c.bg_diff_change = darken(c.blue, 0.07, c.base)
-  c.bg_diff_delete = darken(c.red, 0.18, c.base)
-  c.bg_diff_text = darken(c.blue, 0.30, c.base)
-  c.bg_search = darken(c.sky, 0.30, c.base)
-  c.bg_incsearch = darken(c.sky, 0.90, c.base)
-
-  local mod = {
-    require("themes.decaf.schema.editor").get(c, cfg),
-    require("themes.decaf.schema.native_lsp").get(c, cfg),
-    require("themes.decaf.schema.neotree").get(c, cfg),
-    require("themes.decaf.schema.syntax").get(c, cfg),
-    require("themes.decaf.schema.treesitter").get(c),
-    require("themes.decaf.schema.semantic_tokens").get(c),
-    require("themes.decaf.schema.blink").get(c),
-  }
-
-  local all_highlights = {}
-  for _, m in pairs(mod) do
-    for n, a in pairs(m) do all_highlights[n] = a end
-  end
-
-  local hl = api.nvim_set_hl
-  for n, a in pairs(all_highlights) do hl(0, n, a) end
+local all_highlights = {}
+for _, m in pairs(mod) do
+  for n, a in pairs(m) do all_highlights[n] = a end
 end
 
-local ok, err = pcall(apply_highlights)
-if not ok then
-  vim.notify("Theme loading failed: " .. err, vim.log.levels.ERROR)
-end
+local hl = api.nvim_set_hl
+for n, a in pairs(all_highlights) do hl(0, n, a) end
+
 
 local term = require("themes.decaf.schema.terminal").get(c)
 for i, color in pairs(term) do
