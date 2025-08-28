55bdcae3de6fd19f3f4b68a5b02537fad1c686aa
erikbloodshed <erik.bloodshed@hotmail.com>
2025-08-28 14:37:00 +0800

    refactor: Colorscheme simplify

--- lua/themes/decaf/init.lua
+++ lua/themes/decaf/init.lua
@@ -17,12 +17,12 @@ return {
       },
     }
 
-    local c         = require("themes.decaf.colors")
-    local hls       = require("themes.decaf.schema.schema").get(c, s)
+    local c         = require("themes.decaf._colors")
+    local hls       = require("themes.decaf._schema").get(c, s)
     local hl        = api.nvim_set_hl
     for n, a in pairs(hls) do hl(0, n, a) end
 
-    local t = require("themes.decaf.schema.terminal").get(c)
+    local t = require("themes.decaf._term").get(c)
     for i, k in pairs(t) do
       vim.g[i] = k
     end
