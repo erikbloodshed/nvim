5dad91120e607a4408b59529090fe4990579242d
xenyan <xenyan.matulac@gmail.com>
2025-09-02 16:54:40 +0800

    modified:   lua/quickrun/init.lua

--- lua/quickrun/init.lua
+++ lua/quickrun/init.lua
@@ -1,22 +1,27 @@
 local M = {}
 
+M.config = {}
+
 M.setup = function(cfg)
   local ft = vim.bo.filetype
-  local config = require("quickrun.config").filetype[ft]
+  M.config = require("quickrun.config").filetype[ft]
 
-  if not config then
+  if not M.config then
     vim.notify("Unsupported filetype")
     return
   end
 
   local user_config = cfg and cfg.filetype[ft]
   if user_config then
-    config = vim.tbl_extend("force", config, user_config)
+    M.config = vim.tbl_extend("force", M.config, user_config)
   end
 
-  if config.execution_model == "compiled" then
-  elseif config.execution_model == "assembled" then
-  elseif config.execution_model == "interpreted" then
+  local run_command = nil
+  if M.config.execution_model == "compiled" then
+    run_command = function()
+    end
+  elseif M.config.execution_model == "assembled" then
+  elseif M.config.execution_model == "interpreted" then
   else
   end
 end
