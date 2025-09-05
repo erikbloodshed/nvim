255ec1f61b33ddcbfc1d3ab4d6ab092fb57d6bf2
erikbloodshed <erik.bloodshed@hotmail.com>
2025-09-06 00:59:17 +0800

    refactor(colorscheme): Some small changes to decaf

--- lua/themes/decaf/init.lua
+++ lua/themes/decaf/init.lua
@@ -11,22 +11,14 @@ return {
 
     local s = {
       transparency = false,
-      float = {
-        transparent = false,
-        solid = true,
-      },
+      float = { transparent = false, solid = true, },
     }
 
     local c = require("themes.decaf._colors")
     local hls = require("themes.decaf._schema").get(c, s)
     local hl = api.nvim_set_hl
-    for n, a in pairs(hls) do
-      hl(0, n, a)
-    end
-
+    for n, a in pairs(hls) do hl(0, n, a) end
     local t = require("themes.decaf._term").get(c)
-    for i, k in pairs(t) do
-      vim.g[i] = k
-    end
+    for i, k in pairs(t) do vim.g[i] = k end
   end,
 }
