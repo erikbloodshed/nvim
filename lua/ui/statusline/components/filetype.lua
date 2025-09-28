d259e524ee0d9697aa61d1ad7f084d9792453963
erikbloodshed <erik.bloodshed@hotmail.com>
2025-09-28 15:31:19 +0800

    new file:   components/filetype.lua

---
+++ lua/ui/statusline/components/filetype.lua
@@ -0,0 +1,7 @@
+return {
+  cache_keys = { "filetype" },
+  render = function(ctx, apply_hl)
+    local ft = ctx.filetype ~= "" and ctx.filetype or "none"
+    return ctx.hl_rule(ft, "StatusLine", apply_hl)
+  end,
+}
